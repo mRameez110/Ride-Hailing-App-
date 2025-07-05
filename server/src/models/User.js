47fc98d76309000c9291c5de1ad00c6d54f0d791
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-07-05 23:50:10 +0500

    user model

---
+++ server/src/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["passenger", "driver"],
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (plainPassword) {
+  return await bcrypt.compare(plainPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
