eee267d13d4b77c38483983094203dcbd9b763ef
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-07-06 00:48:07 +0500

    Login Service

--- server/src/services/authServices.js
+++ server/src/services/authServices.js
@@ -6,13 +6,42 @@ const generateToken = (user) => {
   });
 };
 
-const registerService = async (req) => {
-  const { name, email, password, type } = req.body;
+const registerService = async (data) => {
+  const { name, email, password, type } = data.body;
 
   const existing = await User.findOne({ email });
   if (existing) return res.status(400).json({ message: "User already exists" });
 
   const user = await User.create({ name, email, password, type });
-
   return { newUser: user };
+
 };
+
+
+const loginService = async (data) => {
+  const { email, password } = data;
+  const user = await User.findOne({ email });
+  if (!user) {
+    const err = new Error("Invalid credentials");
+    err.statusCode = 400;
+    throw err;
+  }
+
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    const err = new Error("Invalid credentials");
+    err.statusCode = 400;
+    throw err;
+  }
+
+  const token = generateToken(user);
+
+  const userWithoutPassword = {
+    id: user._id,
+    name: user.name,
+    type: user.type,
+  };
+
+  return { user: userWithoutPassword, token };
+};
+
