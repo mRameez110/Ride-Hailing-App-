79a9a660ce19662766c552c0ff3a8e038c7ddedc
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-07-06 01:50:22 +0500

    Update Ride tatus Service

--- server/src/services/rideService.js
+++ server/src/services/rideService.js
@@ -24,6 +24,24 @@ const getSingleRideService = async (rideId) => {
   return ride;
 };
 
+const updateRideStatusService = async (rideId, userId, status) => {
+  const ride = await Ride.findById(rideId);
+  if (!ride) throw new Error("Ride not found");
+
+  if (ride.driver && ride.driver.toString() !== userId.toString()) {
+    throw new Error("Not authorized to update this ride");
+  }
+
+
+  if (status === "Accepted") {
+    ride.driver = userId;
+  }
+
+  ride.status = status;
+  await ride.save();
+  return ride;
+};
+
 module.exports = {
   requestRideService,
   getRideHistoryService,
