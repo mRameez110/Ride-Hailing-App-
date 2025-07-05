802da3b80853988e1dfcb78e5e2824a89c7983b7
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-07-06 02:29:26 +0500

    Add authMiddleware to protected routes

--- server/src/routes/rideRoutes.js
+++ server/src/routes/rideRoutes.js
@@ -3,12 +3,13 @@ const {
   requestRide,
   getRideHistory,
 } = require("../controllers/rideController");
+const checkAuth = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.post("/", requestRide);
-router.get("/", getRideHistory);
-router.get("/:id", getSingleRide);
-router.patch("/:id/status", updateRideStatus);
+router.post("/", checkAuth, requestRide);
+router.get("/", checkAuth, getRideHistory);
+router.get("/:id", checkAuth, getSingleRide);
+router.patch("/:id/status", checkAuth, updateRideStatus);
 
 module.exports = router;
