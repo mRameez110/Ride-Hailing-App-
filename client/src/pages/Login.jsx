62eb0a6df821b33cbdd112517b8664878c0df91c
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-07-06 12:36:02 +0500

    Login Page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { loginUser } from "../utils/authService";
+
+const Login = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      const res = await loginUser(formData);
+      login(res.user, res.token);
+      navigate("/dashboard");
+    } catch (err) {
+      setError(err?.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md p-6 rounded w-full max-w-md"
+      >
+        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+
+        {error && (
+          <p className="mb-4 text-red-600 text-sm bg-red-100 p-2 rounded">
+            {error}
+          </p>
+        )}
+
+        <div className="mb-4">
+          <label className="block mb-1 text-gray-700">Email</label>
+          <input
+            type="email"
+            name="email"
+            required
+            value={formData.email}
+            onChange={handleChange}
+            className="w-full border px-3 py-2 rounded outline-none focus:ring focus:ring-blue-300"
+          />
+        </div>
+
+        <div className="mb-6">
+          <label className="block mb-1 text-gray-700">Password</label>
+          <input
+            type="password"
+            name="password"
+            required
+            value={formData.password}
+            onChange={handleChange}
+            className="w-full border px-3 py-2 rounded outline-none focus:ring focus:ring-blue-300"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded font-semibold"
+        >
+          Login
+        </button>
+
+        <p className="mt-4 text-sm text-center">
+          you do not have an account?{" "}
+          <a href="/register" className="text-blue-600 hover:underline">
+            Register
+          </a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
